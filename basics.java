c78a9cfe7e2e68699d3f70d7905b99fa4fb328dd
GuliaScientist <rakesh.gulia@gmail.com>
2019-08-05 06:51:40 -0500

    Add files via upload

---
+++ basics.java
@@ -0,0 +1,30 @@
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class basics extends base {
+
+	//public static void main(String[] args) throws MalformedURLException
+//	{
+//		AndroidDriver<AndroidElement> driver=capabilities();
+		//driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		
+		//driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
+		//driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
+		//driver.findElementById("android:id/checkbox").click();
+			
+	//	
+	//	driver.findElementByXPath("//android.widget.RelativeLayout[2]").click();
+//	}
+	
+	
+	
+	
+}
+
---
+++ ecommerceCart.java
@@ -0,0 +1,68 @@
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import junit.framework.Assert;
+
+/**
+ * @author sonia
+ *
+ */
+public class ecommerceCart extends base {
+	
+	/*
+	 * public static void main(String[] args) throws MalformedURLException,
+	 * InterruptedException {
+	 * 
+	 * AndroidDriver<AndroidElement> driver=capabilities();
+	 * 
+	 * 
+	 * driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	 * 
+	 * driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).
+	 * sendKeys("Sonia Gulia");
+	 * 
+	 * driver.hideKeyboard();
+	 * 
+	 * driver.findElement(By.xpath("//*[@text='Female']")).click();
+	 * 
+	 * driver.findElement(By.id("android:id/text1")).click();
+	 * 
+	 * 
+	 * driver.
+	 * findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"India\"));"
+	 * );
+	 * 
+	 * driver.findElement(By.xpath("//*[@text='India']")).click();
+	 * 
+	 * driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).
+	 * click();
+	 * 
+	 * 
+	 * driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	 * driver.findElement(By.xpath("//*[@text='Female']")).click();
+	 * 
+	 * driver.findElement(By.id("android:id/text1")).click();
+	 * 
+	 * 
+	 * driver.
+	 * findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"India\"));"
+	 * );
+	 * 
+	 * driver.findElement(By.xpath("//*[@text='India']")).click();
+	 * 
+	 * driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).
+	 * click();
+	 * 
+	 * String
+	 * toastMessage=driver.findElement(By.xpath("//android.widget.Toast[1]")).
+	 * getAttribute("name");
+	 * 
+	 * System.out.println(toastMessage);
+	 * 
+	 * Assert.assertEquals("Please enter your name", toastMessage);//Actual
+	 * validation }
+	 */
+}
